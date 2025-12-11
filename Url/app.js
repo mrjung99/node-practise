9fa57b5063f2672bce6d1108cbe327791f4b3c7c
zeroCool <jungdaulat99@gmail.com>
2025-12-11 11:54:54 +0545

    url shortner

--- Url/app.js
+++ Url/app.js
@@ -1,8 +1,10 @@
-import { readFile } from "fs/promises"
+import { readFile, writeFile } from "fs/promises"
 import http from "http"
 import path from "path"
+import crypto from "crypto"
 
 const port = 3000
+const DATA_PATH = path.join("data", "links.json")
 
 const serveFile = async (res, filePath, contentType) => {
     try {
@@ -15,13 +17,60 @@ const serveFile = async (res, filePath, contentType) => {
     }
 }
 
+const getLinks = async () => {
+    try {
+        const data = await readFile(DATA_PATH, "utf-8")
+        return JSON.parse(data)
+    } catch (error) {
+        if (error.code === "ENOENT") {
+            await writeFile(DATA_PATH, JSON.stringify({}))
+            return {}
+        }
+        throw error
+    }
+}
+
+const saveLinks = async (links) => {
+    await writeFile(DATA_PATH, JSON.stringify(links))
+}
+
 const server = http.createServer(async (req, res) => {
+    const links = await getLinks()
+
     if (req.method === "GET") {
         if (req.url === "/") {
             return serveFile(res, path.join("public", "index.html"), "text/html")
         } else if (req.url === "/style.css") {
-            return serveFile(res, path.join("public", "style.css"), "text/plain")
+            return serveFile(res, path.join("public", "style.css"), "text/css")
         }
+    }
+
+    if (req.method === "POST" && req.url === "/shortend") {
+        let body = ""
+
+        req.on("data", (chunk) => (body += chunk))
+
+        req.on("end", async () => {
+            const { url, shortKey } = JSON.parse(body)
+
+            if (!url) {
+                res.writeHead(400, { "Content-Type": "text/plain" })
+                return res.end("URL is required")
+            }
+
+            const finalShortCode = shortKey || crypto.randomBytes(4).toString("hex")
+
+            if (links[finalShortCode]) {
+                res.writeHead(400, { "Content-Type": "text/plain" })
+                return res.end("Short code is already taken type another one")
+            }
+
+            links[finalShortCode] = url;
+            await saveLinks(links)
+
+            res.writeHead(200, { "Content-Type": "application/json" })
+            res.end(JSON.stringify({ success: true, shortCode: finalShortCode }))
+        })
     }
 })
 
--- pathmodule.js
+++ pathmodule.js
@@ -1,16 +1,7 @@
-const { log } = require("console");
 const path = require("path");
 
-const filePath = path.join("folder", "student", "student.txt");
+const filePath = path.join("folder", "student.txt");
+console.log(filePath);
 
-const base = path.basename(filePath);
-const dir = path.dirname(filePath);
-const resolve = path.resolve(filePath);
-console.log({ base, dir, resolve });
 
-const path2 = "/tmp/myfile.html"
-const modeule =
-    path.basename('/foo/bar/baz/asdf/quux.html', '.html');
-
-console.log(modeule);
 
