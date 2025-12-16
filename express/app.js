7730f8d19f38a2b21c95158c26b9482fb90d552c
zeroCool <jungdaulat99@gmail.com>
2025-12-16 18:45:02 +0545

    serving static file

--- express/app.js
+++ express/app.js
@@ -2,18 +2,25 @@
 //* to create a server
 //* to create and setup .env file
 
-
 import express from "express"
 import { PORT } from "./env.js"
+import path from "path"
+
 const app = express()
 
-app.get("/", (req, res) => {
-    res.send("Hello express js")
-})
+//this will send the html,css and js file
+const absolutePath = path.join(import.meta.dirname, "public")
+// app.use(express.static(absolutePath)) or
+app.use(express.static("public"))
 
-app.get("/about", (req, res) => {
-    res.send(`<h1>Hello from about page</h1>`)
-})
+// app.get("/", (req, res) => {
+//     // console.log(import.meta.dirname);
+//     // console.log(import.meta.filename);
+
+//     const pagePath = path.join(import.meta.dirname, "public", "index.html")
+//     res.sendFile(pagePath)
+//     // res.send("Hello express js")
+// })
 
 // const PORT = PORT || 3000
 app.listen(PORT, () => {
