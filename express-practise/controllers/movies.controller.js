61baa368b3bcd15adf4c2f830f0d0a75eb5f848c
zeroCool <jungdaulat99@gmail.com>
2026-01-07 21:36:45 +0545

    movie: crud operation

--- express-practise/controllers/movies.controller.js
+++ express-practise/controllers/movies.controller.js
@@ -1,6 +1,9 @@
 import fs from "fs";
 
+//read file from data/movies.json
 const movies = JSON.parse(fs.readFileSync("./data/movies.json"));
+
+//============send all movies when get request ===========
 export function getMovies(req, res) {
   res.status(200).json({
     success: true,
@@ -10,10 +13,30 @@ export function getMovies(req, res) {
   });
 }
 
+//---------------get movie with the id-----------
+export function getMovie(req, res) {
+  const id = Number(req.params.id);
+  const findMovie = movies.find((m) => m.id === id);
+
+  if (!findMovie) {
+    return res
+      .status(404)
+      .json({ success: false, message: `Movie with the id ${id} not found!!` });
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Movie found.",
+    data: {
+      movie: findMovie,
+    },
+  });
+}
+
+// ============ add or post movie ===============
 export function postMovie(req, res) {
   const id = movies.length + 1;
-  const movie = req.body;
-  movies.push(Object.assign({ id: id }, movie));
+  movies.push({ id, ...req.body });
 
   fs.writeFile("./data/movies.json", JSON.stringify(movies), (err) => {
     if (err) {
@@ -23,5 +46,67 @@ export function postMovie(req, res) {
     }
 
     res.status(201).json({ success: true, message: "Created" });
+  });
+}
+
+//=============update movie (patch)================
+export function updateMovie(req, res) {
+  const id = Number(req.params.id);
+
+  let findMovie = movies.find((m) => m.id === id);
+  if (!findMovie) {
+    return res.status(404).json({
+      success: false,
+      message: `Movies with the id ${id} not found!!`,
+    });
+  }
+
+  const index = movies.indexOf(findMovie);
+  //we have to pass req.body with spread operator coz body is the properties of req object so, body:req.body will work or body = req.body and passing body with {..findMovie, body} it will works also
+  const updatedMovie = { ...findMovie, ...req.body };
+  movies[index] = updatedMovie;
+
+  fs.writeFile("./data/movies.json", JSON.stringify(movies), (err) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ success: false, message: "Something went wrong !!" });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Movie updated successfully!!",
+      data: {
+        movie: updateMovie,
+      },
+    });
+  });
+}
+
+//============== delete movie=============
+export function deleteMovie(req, res) {
+  const id = Number(req.params.id);
+  const movieToDelete = movies.find((m) => m.id === id);
+
+  if (!movieToDelete) {
+    res
+      .status(404)
+      .json({ success: false, message: `Movie with the id ${id} not found!!` });
+  }
+
+  const index = movies.indexOf(movieToDelete);
+  movies.splice(index, 1);
+
+  fs.writeFile("./data/movies.json", JSON.stringify(movies), (err) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ success: false, message: "Something went wrong !!" });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Movie deleted successfully!!",
+    });
   });
 }
--- express-practise/routes/movie.route.js
+++ express-practise/routes/movie.route.js
@@ -1,6 +1,16 @@
 import express from "express";
-import { getMovies, postMovie } from "../controllers/movies.controller.js";
+import {
+  deleteMovie,
+  getMovie,
+  getMovies,
+  postMovie,
+  updateMovie,
+} from "../controllers/movies.controller.js";
+
 export const movieRoute = express.Router();
 
 movieRoute.get("/", getMovies);
+movieRoute.get("/:id", getMovie);
 movieRoute.post("/", postMovie);
+movieRoute.patch("/:id", updateMovie);
+movieRoute.delete("/:id", deleteMovie);
